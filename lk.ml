b1d671ac662b3c04e23b457a1f958952cd834bd8
John Li <johnli0135@gmail.com>
2020-05-22 13:54:59 -0400

    Impl nary sums

--- lk.ml
+++ lk.ml
@@ -16,7 +16,7 @@ let ( *> ) m1 m2 = let* _ = m1 in m2
 *)
 type ty
   = Void | Unit
-  | Sum of ty * ty
+  | Sum of ty list
   | Prod of ty * ty
   | Neg of ty
   | Vdash of ty list * ty list
@@ -30,6 +30,10 @@ type 'a ctx = 'a -> (ty, [`NotFound]) result
 let empty : 'a ctx = fun _ -> Err `NotFound
 let extend (x : 'a) (t : ty) (c : 'a ctx) : 'a ctx =
   fun x' -> if x = x' then Ok t else c x'
+
+let extends (vs : 'a list) (ts : ty list) (vts : 'a ctx) : 'a ctx =
+  let open List in
+  fold_right (fun (v, t) vts -> extend v t vts) (combine vs ts) vts
 
 (* Terms *)
 
@@ -50,10 +54,10 @@ type exp
   | Pair of covar * covar * exp * covar * exp
   | Unpair of var * var * var * exp
   (* Sums
-     | let [h, j] = k in e
-     | case x (λ y. e₁) (λ z. e₂) *)
-  | Uncase of covar * covar * covar * exp
-  | Case of var * var * exp * var * exp
+     | let [j, ..] = k in e
+     | case x of y -> e | .. end *)
+  | Uncase of covar list * covar * exp
+  | Case of var * (var * exp) list
   (* Negation
      | let x -> k in e
      | let x <- k in e *)
@@ -92,8 +96,8 @@ let as_prod m =
 let as_sum m =
   let* ty = m in
   match ty with
-  | Sum (s, t) -> Ok (s, t)
-  | _ -> Err (`ExGot ("_ + _", ty))
+  | Sum ts -> Ok ts
+  | _ -> Err (`ExGot ("_ + ..", ty))
 
 let as_neg m =
   let* ty = m in
@@ -113,6 +117,8 @@ let fresh =
     x := !x + 1;
     !x
   in fresh
+
+let mapM_ f xs = List.fold_left (fun m x -> m *> f x) (Ok ()) xs
 
 (* Γ ⊢ e ⊣ Δ *)
 let rec check (vars : var ctx) (e : exp) (covars : covar ctx) =
@@ -157,19 +163,21 @@ let rec check (vars : var ctx) (e : exp) (covars : covar ctx) =
   | Unpair (y, z, x, e) ->
     let* (s, t) = as_prod (find_var x vars) in
     check (extend y s (extend z t vars)) e covars
-  (*        Γ ⊢ e ⊣ h : σ, j : τ, Δ
-     ——————————————————————————————————————
-     Γ ⊢ let [h, j] = k in e ⊣ k : σ + τ, Δ *)
-  | Uncase (h, j, k, e) ->
-    let* (s, t) = as_sum (find_covar k covars) in
-    check vars e (extend h s (extend j t covars))
-  (*  Γ, y : σ ⊢ e₁ ⊣ Δ    Γ, z : τ ⊢ e₂ ⊣ Δ
+  (*           Γ ⊢ e ⊣ j : τ, .., Δ
+     ————————————————————————————————————————
+     Γ ⊢ let [j, ..] = k in e ⊣ k : τ + .., Δ *)
+  | Uncase (js, k, e) ->
+    let* ts = as_sum (find_covar k covars) in
+    if List.(length js <> length ts) then Err (`UncaseArity (js, ts)) else
+    check vars e (extends js ts covars)
+  (*          Γ, y : τ ⊢ e ⊣ Δ    ..
      —————————————————————————————————————————
-     Γ, x : σ + τ ⊢ case x (λ y. e₁) (λ z. e₂) *)
-  | Case (x, y, e1, z, e2) ->
-    let* (s, t) = as_sum (find_var x vars) in
-    check (extend y s vars) e1 covars *>
-    check (extend z t vars) e2 covars
+     Γ, x : τ + .. ⊢ case x of y -> e | .. end *)
+  | Case (x, yes) ->
+    let* ts = as_sum (find_var x vars) in
+    let open List in
+    if length yes <> length ts then Err (`CaseArity (yes, ts)) else
+    mapM_ (fun ((y, e), t) -> check (extend y t vars) e covars) (combine yes ts)
   (*         Γ ⊢ e ⊣ k : τ, Δ
      ————————————————————————————————
      Γ, x : ¬ τ ⊢ let x -> k in e ⊣ Δ *)
@@ -188,9 +196,8 @@ let rec check (vars : var ctx) (e : exp) (covars : covar ctx) =
   | LK (k, xs, js, e) ->
     let* (ss, ts) = as_vdash (find_covar k covars) in
     let open List in
-    if length xs = length ss then Err (`LKArgArity (xs, ss)) else
-    if length js = length ts then Err (`LKContArity (js, ts)) else
-    let extends vs ts vts = fold_right (fun (v, t) vts -> extend v t vts) (combine vs ts) vts in
+    if length xs <> length ss then Err (`LKArgArity (xs, ss)) else
+    if length js <> length ts then Err (`LKContArity (js, ts)) else
     check (extends xs ss vars) e (extends js ts covars)
   (*          Γ ⊢ e₁ ⊣ k : σ, Δ    ..    Γ, y : τ ⊢ e₂ ⊣ Δ    ..
      ————————————————————————————————————————————————————————————————————
@@ -198,9 +205,8 @@ let rec check (vars : var ctx) (e : exp) (covars : covar ctx) =
   | LKApp (x, kes, yes) ->
     let* (ss, ts) = as_vdash (find_var x vars) in
     let open List in
-    if length kes = length ss then Err (`LKAppArgArity (kes, ss)) else
-    if length yes = length ts then Err (`LKAppContArity (yes, ts)) else
-    let mapM_ f xs = fold_left (fun m x -> m *> f x) (Ok ()) xs in
+    if length kes <> length ss then Err (`LKAppArgArity (kes, ss)) else
+    if length yes <> length ts then Err (`LKAppContArity (yes, ts)) else
     mapM_ (fun ((k, e), s) -> check vars e (extend k s covars)) (combine kes ss) *>
     mapM_ (fun ((y, e), t) -> check (extend y t vars) e covars) (combine yes ts)
 
@@ -212,9 +218,8 @@ type lc
   | App of lc * lc
   | LTrivial
   | LAbsurd of lc
-  | In1 of lc
-  | In2 of lc
-  | LCase of lc * string * lc * string * lc
+  | In of int * lc
+  | LCase of lc * (string * lc) list
   | LPair of lc * lc
   | LUnpair of string * string * lc * lc
 
@@ -244,13 +249,15 @@ let rec c_exp : exp -> lc = function
     LPair (v, w)))
   (* [[ let (y, z) = x in e ]] = let (y, z) = x in [[e]] *)
   | Unpair (y, z, x, e) -> LUnpair (s_x y, s_x z, c_x x, c_exp e)
-  (* [[ let [h, j] = k in e ]] = (λ h j. [[e]]) (λ v. k (ι₁ v)) (λ w. k (ι₂ w)) *)
-  | Uncase (h, j, k, e) ->
-    (klam h (klam j (c_exp e))
-      $ vlam (fun v -> c_k k $ In1 v))
-      $ vlam (fun w -> c_k k $ In2 w)
+  (* [[ let [j, ..] = k in e ]] = (λ j .. . [[e]]) (λ v. k (ι₁ v)) .. *)
+  | Uncase (js, k, e) ->
+    let open List in
+    snd (fold_left
+      (fun (n, e) j -> (n + 1, e $ vlam (fun v -> c_k k $ In (n, v))))
+      (0, fold_right klam js (c_exp e))
+      js)
   (* [[ case x (λ y. e₁) (λ z. e₂) ]] = case x of ι₁ y -> [[e₁]] | ι₂ z -> [[e₂]] end *)
-  | Case (x, y, e1, z, e2) -> LCase (c_x x, s_x y, c_exp e1, s_x z, c_exp e2)
+  | Case (x, yes) -> LCase (c_x x, List.map (fun (y, e) -> (s_x y, c_exp e)) yes)
   (* [[ let x -> k in e ]] = (λ k. [[e]]) x *)
   | ToCo (x, k, e) -> klam k (c_exp e) $ c_x x
   (* [[ let x <- k in e ]] = k (λ x. [[e]]) *)
@@ -267,8 +274,11 @@ let rec c_exp : exp -> lc = function
 (* Pretty-printing for sequent calculus terms *)
 
 let cat = String.concat ""
-let unwords_by f xs = String.concat " " (List.map f xs)
-let commas_by f xs = String.concat ", " (List.map f xs)
+let cat_map sep f xs = String.concat sep (List.map f xs)
+let unwords_by f xs = cat_map " " f xs
+let commas_by f xs = cat_map ", " f xs
+let arms_by f xs = cat_map " | " f xs
+let uncurry f (x, y) = f x y
 
 let p_x (Var x) = "x" ^ string_of_int x
 let p_k (Covar k) = "k" ^ string_of_int k
@@ -276,7 +286,7 @@ let p_k (Covar k) = "k" ^ string_of_int k
 let rec p_ty : ty -> string = function
   | Void -> "0"
   | Unit -> "1"
-  | Sum (s, t) -> cat ["("; p_ty s; " + "; p_ty t; ")"]
+  | Sum ts -> cat ["("; String.concat " + " (List.map p_ty ts); ")"]
   | Prod (s, t) -> cat ["("; p_ty s; " × "; p_ty t; ")"]
   | Neg t -> cat ["¬"; p_ty t]
   | Vdash (ss, ts) -> cat ["("; commas_by p_ty ss; " ⊢ "; commas_by p_ty ts; ")"]
@@ -291,8 +301,8 @@ let rec p_exp : exp -> string = function
   | Trivial k -> cat ["["; p_k k; "]★"]
   | Pair (k, h, e1, j, e2) -> cat ["["; p_k k; "]("; p_kappa h e1; ", "; p_kappa j e2; ")"]
   | Unpair (y, z, x, e) -> cat ["let ("; p_x y; ", "; p_x z; ") = "; p_x x; " in "; p_exp e]
-  | Uncase (h, j, k, e) -> cat ["let ["; p_k h; ", "; p_k j; "] = "; p_k k; " in "; p_exp e]
-  | Case (x, y, e1, z, e2) -> cat ["case "; p_x x; " of "; p_arm y e1; " | "; p_arm z e2; " end"]
+  | Uncase (js, k, e) -> cat ["let ["; commas_by p_k js; "] = "; p_k k; " in "; p_exp e]
+  | Case (x, yes) -> cat ["case "; p_x x; " of "; arms_by (uncurry p_arm) yes; " end"]
   | ToCo (x, k, e) -> cat ["let "; p_x x; " -> "; p_k k; " in "; p_exp e]
   | OfCo (x, k, e) -> cat ["let "; p_x x; " <- "; p_k k; " in "; p_exp e]
   | LK (k, xs, js, e) ->
@@ -313,12 +323,9 @@ let rec p_lc : lc -> string = function
   | App (e1, e2) -> cat ["("; p_lc e1; " "; p_lc e2; ")"]
   | LTrivial -> "★"
   | LAbsurd e -> cat ["case "; p_lc e; " of end"]
-  | In1 e -> cat ["(ι₁ "; p_lc e; ")"]
-  | In2 e -> cat ["(ι₂ "; p_lc e; ")"]
-  | LCase (e, x, e1, y, e2) ->
-    cat ["case "; p_lc e;
-      " of ι₁ "; x; " -> "; p_lc e1;
-      " | ι₂ "; y; " -> "; p_lc e2; " end"]
+  | In (n, e) -> cat ["(ι"; string_of_int n; " "; p_lc e; ")"]
+  | LCase (e, yes) ->
+    cat ["case "; p_lc e; " of "; arms_by (fun (x, e) -> cat [x; " -> "; p_lc e]) yes; " end"]
   | LPair (e1, e2) -> cat ["("; p_lc e1; ", "; p_lc e2; ")"]
   | LUnpair (x, y, e1, e2) -> cat ["let ("; x; ", "; y; ") = "; p_lc e1; " in "; p_lc e2]
 
